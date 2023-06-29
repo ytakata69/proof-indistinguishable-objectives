e7ff88dbeff240245909b0a2d10c4e0175fb99e8
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2023-06-29 23:51:01 +0900

    Modify the definition of ObjGW

--- ind_obj.v
+++ ind_obj.v
@@ -102,15 +102,41 @@ Infix "∪" := (Union U)         (at level 50, left associativity).
 Infix "⊆" := (Included U)      (at level 55, no associativity).
 Notation "x ∈ S" := (In U S x) (at level 55, no associativity).
 Notation "¬ x" := (Complement U x) (at level 35).
+Notation "∅" := (Empty_set U).
 
 Definition Omega := Ensemble Play.
-Variable winnablep : Ensemble Omega.
 
 Infix ".∩" := (Intersection Omega)  (at level 40, left associativity).
 Infix ".∪" := (Union Omega)         (at level 50, left associativity).
 Infix ".⊆" := (Included Omega)      (at level 55, no associativity).
 Notation "x .∈ S" := (In Omega S x) (at level 55, no associativity).
 Notation ".¬ x" := (Complement Omega x) (at level 35).
+
+Variable winnablep : Ensemble Omega.
+Hypothesis empty_objective_is_not_winnable :
+  ∅ .∈ .¬ winnablep.
+
+(* Original definitions of the Obj's *)
+Inductive ObjPW' (Op : Omega) (rho : Play) : Ensemble Omega :=
+| ObjPW'_intro : forall O,
+  (rho ∈ O /\ rho ∈ Op) \/ (rho ∈ ¬ O /\ rho ∈ ¬ Op)
+  -> O .∈ ObjPW' Op rho.
+
+Inductive ObjGW' (Op : Omega) (rho : Play) : Ensemble Omega :=
+| ObjGW'_intro : forall O,
+  (rho ∈ Op /\ O <> ∅) \/ (rho ∈ ¬ Op /\ O .∈ .¬ winnablep)
+  -> O .∈ ObjGW' Op rho.
+
+Inductive ObjPG' (Op : Omega) (rho : Play) : Ensemble Omega :=
+| ObjPG'_intro : forall O,
+  (rho ∈ O /\ O <> ∅) \/ (rho ∈ ¬ O /\ O .∈ .¬ winnablep)
+  -> O .∈ ObjPG' Op rho.
+
+Inductive ObjPGW' (Op : Omega) (rho : Play) : Ensemble Omega :=
+| ObjPGW'_intro : forall O,
+  (rho ∈ O /\ rho ∈ Op /\ O <> ∅) \/
+  (rho ∈ ¬ O /\ rho ∈ ¬ Op /\ O .∈ .¬ winnablep)
+  -> O .∈ ObjPGW' Op rho.
 
 (* Definitions of the Obj's *)
 Inductive ObjPW (Op : Omega) (rho : Play) : Ensemble Omega :=
@@ -119,7 +145,8 @@ Inductive ObjPW (Op : Omega) (rho : Play) : Ensemble Omega :=
 
 Inductive ObjGW (Op : Omega) (rho : Play) : Ensemble Omega :=
 | ObjGW_intro : forall O,
-  (O .∈ winnablep -> rho ∈ Op) -> O .∈ ObjGW Op rho.
+  (O .∈ winnablep -> rho ∈ Op) /\
+  (O = ∅ -> rho ∈ ¬ Op) -> O .∈ ObjGW Op rho.
 
 Inductive ObjPG (Op : Omega) (rho : Play) : Ensemble Omega :=
 | ObjPG_intro : forall O,
@@ -129,6 +156,176 @@ Inductive ObjPGW (Op : Omega) (rho : Play) : Ensemble Omega :=
 | ObjPGW_intro : forall O,
   rho ∈ (O ∩ Op) ∪ (¬ O ∩ ¬ Op) /\
   (O .∈ winnablep -> rho ∈ O ∩ Op) -> O .∈ ObjPGW Op rho.
+
+(*
+ * Showing the original and the simplified
+ * definitions are the same.
+ *)
+Theorem ObjPW_eq_ObjPW' :
+  forall Op rho,
+  ObjPW Op rho = ObjPW' Op rho.
+Proof.
+  intros Op rho.
+  apply Extensionality_Ensembles.
+  unfold Same_set, Included, In.
+  split;
+  intros O H.
+  - (* ⊆ *)
+  apply ObjPW'_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct H' as [rho H | rho H].
+  + (* when rho ∈ O ∩ Op *)
+  destruct H as [rho H1 H2].
+  left; split; assumption.
+  + (* when rho ∈ ¬ O ∩ ¬ Op *)
+  destruct H as [rho H1 H2].
+  right; split; assumption.
+  - (* ⊇ *)
+  apply ObjPW_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct H' as [H | H].
+  + (* when rho ∈ O /\ rho ∈ Op *)
+  destruct H as [H1 H2].
+  apply Union_introl.
+  now apply Intersection_intro.
+  + (* when rho ∈ ¬ O /\ rho ∈ ¬ Op *)
+  destruct H as [H1 H2].
+  apply Union_intror.
+  now apply Intersection_intro.
+Qed.
+
+Theorem ObjGW_eq_ObjGW' :
+  forall Op rho,
+  ObjGW Op rho = ObjGW' Op rho.
+Proof.
+  intros Op rho.
+  apply Extensionality_Ensembles.
+  unfold Same_set, Included, In.
+  split;
+  intros O H.
+  - (* ⊆ *)
+  apply ObjGW'_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct H' as [H1 H2].
+  destruct (In_dec _ Op rho) as [Hop | Hop].
+  + (* when rho ∈ Op *)
+  left. split; [assumption |].
+  intros Hemp.
+  apply H2 in Hemp.
+  now apply Hemp in Hop.
+  + (* when rho ∈ ¬ Op *)
+  right. split; [assumption |].
+  intros Hwin.
+  apply Hop.
+  now apply H1.
+  - (* ⊇ *)
+  apply ObjGW_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct H' as [H | H].
+  + (* when rho ∈ Op /\ O <> ∅ *)
+  destruct H as [H1 H2].
+  split; intros Ho; [assumption |].
+  now apply H2 in Ho.
+  + (* when rho ∈ ¬ Op /\ O .∈ .¬ winnablep *)
+  destruct H as [H1 H2].
+  split; intros Ho; [| assumption].
+  now apply H2 in Ho.
+Qed.
+
+Theorem ObjPG_eq_ObjPG' :
+  forall Op rho,
+  ObjPG Op rho = ObjPG' Op rho.
+Proof.
+  intros Op rho.
+  apply Extensionality_Ensembles.
+  unfold Same_set, Included, In.
+  split;
+  intros O H.
+  - (* ⊆ *)
+  apply ObjPG'_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct (In_dec _ O rho) as [Ho | Ho].
+  + (* when rho ∈ O *)
+  left. split; [assumption |].
+  intros Hemp.
+  rewrite Hemp in Ho.
+  inversion Ho.
+  + (* when rho ∈ ¬ O *)
+  right. split; [assumption |].
+  intros Hwin.
+  apply H' in Hwin.
+  now apply Ho in Hwin.
+  - (* ⊇ *)
+  apply ObjPG_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct H' as [H | H].
+  + (* when rho ∈ O /\ O <> ∅ *)
+  destruct H as [H1 H2].
+  now intros Hwin.
+  + (* when rho ∈ ¬ O /\ O .∈ .¬ winnablep *)
+  destruct H as [H1 H2].
+  intros Hwin.
+  now apply H2 in Hwin.
+Qed.
+
+Theorem ObjPGW_eq_ObjPGW' :
+  forall Op rho,
+  ObjPGW Op rho = ObjPGW' Op rho.
+Proof.
+  intros Op rho.
+  apply Extensionality_Ensembles.
+  unfold Same_set, Included, In.
+  split;
+  intros O H.
+  - (* ⊆ *)
+  apply ObjPGW'_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct H' as [H1 H2].
+  destruct H1 as [rho Hop | rho Hop].
+  + (* when rho ∈ O ∩ Op *)
+  destruct Hop as [rho Ho Hop].
+  left. split; [| split]; try assumption.
+  intros Hemp.
+  rewrite Hemp in Ho.
+  inversion Ho.
+  + (* when rho ∈ ¬ O ∩ ¬ Op *)
+  destruct Hop as [rho Ho Hop].
+  right. split; [| split]; try assumption.
+  intros Hwin.
+  apply H2 in Hwin.
+  destruct Hwin as [rho Hwin _].
+  now apply Ho in Hwin.
+  - (* ⊇ *)
+  apply ObjPGW_intro.
+  inversion H as [O' H' EQO'];
+  clear O' EQO' H.
+  destruct H' as [H | H].
+  + (* when rho ∈ O /\ rho ∈ Op /\ O <> ∅ *)
+  destruct H as [H1 [H2 H3]].
+  split.
+  * (* to show rho ∈ O ∩ Op ∪ ¬ O ∩ ¬ Op *)
+  apply Union_introl.
+  now apply Intersection_intro.
+  * (* to show O .∈ winnablep -> rho ∈ O ∩ Op *)
+  intros Hwin.
+  now apply Intersection_intro.
+  + (* when rho ∈ ¬ O /\ rho ∈ ¬ Op /\ O .∈ .¬ winnablep *)
+  destruct H as [H1 [H2 H3]].
+  split.
+  * (* to show rho ∈ O ∩ Op ∪ ¬ O ∩ ¬ Op *)
+  apply Union_intror.
+  now apply Intersection_intro.
+  * (* to show O .∈ winnablep -> rho ∈ O ∩ Op *)
+  intros Hwin.
+  now apply H3 in Hwin.
+Qed.
 
 (*
  * Showing ObjPGW Op rho =
@@ -156,10 +353,16 @@ Proof.
   intros O H.
   destruct H as [O [H1 H2]].
   apply ObjGW_intro.
-  intros Ho.
+  split.
+  - intros Ho.
   specialize (H2 Ho).
   destruct H2 as [x H2 H2'].
   apply H2'.
+  - intros Hemp.
+  destruct H1 as [rho H1 | rho H1].
+  + destruct H1 as [rho H1 _].
+  rewrite Hemp in H1. inversion H1.
+  + now destruct H1 as [rho _ H1].
 Qed.
 
 Lemma included_ObjPGW_ObjPG :
@@ -208,6 +411,7 @@ Proof.
   [apply Union_introl | apply Union_intror]; assumption.
   - (* to show O .∈ winnablep -> rho ∈ O ∩ Op *)
   intros Hw.
+  destruct Hgw as [Hgw _].
   specialize (Hgw Hw).
   specialize (Hpg Hw).
   now apply Intersection_intro.
@@ -281,7 +485,9 @@ Theorem characterization_of_indistinguishable_strategy_GW :
   forall Op calO,
   calO .⊆ ObjGWa Op <->
   outp_sigmap ⊆
-    IntersectionForall (calO .∩ winnablep) (fun O => Op).
+    IntersectionForall (calO .∩ (Singleton _ ∅))
+                       (fun O => ¬ Op)
+      ∩ IntersectionForall (calO .∩ winnablep) (fun O => Op).
 Proof.
   intros Op calO.
   split.
@@ -290,6 +496,10 @@ Proof.
   unfold ObjGWa, Included in Hobj.
   unfold Included.
   intros rho Hrho.
+  apply Intersection_intro.
+  + (* to show outp_sigmap ⊆
+       IntersectionForall (calO .∩ (Singleton _ ∅))
+                          (fun O => ¬ Op) *)
   apply IntersectionForall_intro;
   intros O Ho.
   destruct Ho as [O Ho Ho1].
@@ -297,6 +507,20 @@ Proof.
   destruct Hobj as [O Hobj].
   specialize (Hobj rho Hrho).
   destruct Hobj as [O Hobj].
+  destruct Hobj as [Hobj1 Hobj2].
+  apply Hobj2.
+  destruct Ho1.
+  reflexivity.
+  + (* to show outp_sigmap ⊆
+       IntersectionForall (calO .∩ winnablep) (fun O => Op) *)
+  apply IntersectionForall_intro;
+  intros O Ho.
+  destruct Ho as [O Ho Ho1].
+  specialize (Hobj O Ho).
+  destruct Hobj as [O Hobj].
+  specialize (Hobj rho Hrho).
+  destruct Hobj as [O Hobj].
+  destruct Hobj as [Hobj _].
   apply (Hobj Ho1).
   - (* <- *)
   unfold Included.
@@ -306,9 +530,19 @@ Proof.
   intros rho Hrho.
   specialize (Hout rho Hrho).
   apply ObjGW_intro.
+  destruct Hout as [rho Ho1 Ho2].
+  split.
+  + (* to show O .∈ winnablep -> rho ∈ Op *)
   intros Hw.
-  destruct Hout as [rho Ho1].
-  apply (Ho1 O (Intersection_intro _ _ _ _ Ho Hw)).
+  destruct Ho2 as [rho Ho2].
+  apply (Ho2 O (Intersection_intro _ _ _ _ Ho Hw)).
+  + (* to show O = ∅ -> rho ∈ ¬ Op *)
+  intros He.
+  destruct Ho1 as [rho Ho1].
+  apply (Ho1 O).
+  apply Intersection_intro; [assumption |].
+  rewrite He.
+  apply In_singleton.
 Qed.
 
 Theorem characterization_of_indistinguishable_strategy_PG :
@@ -524,23 +758,50 @@ Qed.
 Theorem characterization_of_winning_IS_GW :
   forall Op calO,
   calO .⊆ ObjGWa Op /\ outp_sigmap ⊆ Op <->
-  outp_sigmap ⊆ Op.
+  outp_sigmap ⊆ Op
+    ∩ IntersectionForall (calO .∩ Singleton _ ∅)
+                         (fun O => ¬ Op).
 Proof.
   intros Op calO.
   split.
   - (* -> *)
-  now intros [_ Hwin].
-  - (* <- *)
-  intros Hinc.
-  split; [| assumption].
-  apply characterization_of_indistinguishable_strategy_GW.
+  intros [His Hwin].
+  apply subset_of_each_is_subset_of_intersection.
+  split; [assumption |].
   unfold Included.
-  unfold Included in Hinc.
-  intros x Hx.
-  specialize (Hinc x Hx).
+  intros rho Hrho.
   apply IntersectionForall_intro.
   intros O Ho.
-  apply Hinc.
+  destruct Ho as [O Ho1 Ho2].
+  inversion Ho2 as [EQO];
+  clear Ho2.
+  unfold Included in His.
+  specialize (His O Ho1).
+  inversion His as [O' His' EQO'];
+  clear O' EQO' His.
+  specialize (His' rho Hrho).
+  inversion His' as [O' His EQO'];
+  clear O' EQO'.
+  destruct His as [His1 His2].
+  apply His2.
+  rewrite EQO.
+  reflexivity.
+  - (* <- *)
+  intros Hinc.
+  apply subset_of_each_is_subset_of_intersection in Hinc.
+  destruct Hinc as [Hinc1 Hinc2].
+  split; [| assumption].
+  apply characterization_of_indistinguishable_strategy_GW.
+  apply subset_of_each_is_subset_of_intersection.
+  unfold Included.
+  unfold Included in Hinc1.
+  unfold Included in Hinc2.
+  split;
+  intros rho Hrho;
+  [specialize (Hinc2 rho Hrho) |
+   specialize (Hinc1 rho Hrho)]; try assumption.
+  apply IntersectionForall_intro.
+  now intros O Ho.
 Qed.
 
 Theorem characterization_of_winning_IS_PG :
